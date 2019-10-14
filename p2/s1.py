3ac30717942ae2e3b326433cea2feb34536cf854
qjj <xxjinxx@qq.com>
2019-10-15 03:21:38 +0800

    second

---
+++ p2/s1.py
@@ -0,0 +1,4 @@
+from p1.f2 import *
+print(a)
+#print(b) __all__里没包含，不能调用
+print(d)
--- p2/s2.py
+++ p2/s2.py
@@ -1,2 +1,6 @@
-import p1.f1
-print(p1.f1.f1)
+import p1.f1 as m   #引用另一个包的变量
+
+from p1.f1 import f1
+#print(p1.f1.f1)
+#print(m.f1)
+print(f1)
